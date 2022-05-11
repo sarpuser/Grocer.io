35c57d9612356709359a3273cc915ff9f917ad98
chasebastian <cbastian@ucsd.edu>
2022-05-11 13:37:04 -0700

    [PY] register_user.py now stores user_id in file

--- rpi/register_user.py
+++ rpi/register_user.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import requests
 import json
 
@@ -8,8 +9,11 @@ import time
 
 
 BUTTON_GPIO = 16
-USER_ID = 0 #TODO: Send this USER_ID to barcode scanner? Socket communication or pipes or what?
-url = "http://164.92.97.79/request_user_id/
+USER_ID = 0
+url = "http://164.92.97.79/request_user_id/"
+
+user_id_file_path = "user_id.txt"
+
 
 
 def request_user_id():
@@ -17,6 +21,15 @@ def request_user_id():
         r = request.get(url, timeout=1) #wait for response for 1 second before timing out
         r = response.json()
         USER_ID = r["user_id"] #need to test if this works, or if I need matchdict or something
+
+        #Save user_id for later use
+        if os.path.exists(user_id_file_path):
+            os.remove(user_id_file_path)
+
+        f = open(user_id_file_path, "w")
+        f.write("USER_ID: " + USER_ID)
+        f.close()
+        
 
     except requests.exceptions.Timeout:
         print("request_user_id restful request timed out (No match ?)")
@@ -30,5 +43,6 @@ if __name__ == "__main__":
 
         GPIO.add_event_detect(BUTTON_GPIO, GPIO.FALLING, 
                 callback=request_user_id, bouncetime=100)   #setup interrupt on pulldown to send request
+        r = request.get(url, timeout=1) #wait for response for 1 second before timing out
     except KeyboardInterrupt:
         exit
