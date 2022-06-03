3a9d9126e3169a6726d051cdf814c7f6181f4162
chasebastian <cbastian@ucsd.edu>
2022-06-02 19:45:27 -0700

    [PY]working python code for pairing device.

--- rpi/register_user.py
+++ rpi/register_user.py
@@ -8,7 +8,7 @@ import RPi.GPIO as GPIO
 import time
 
 
-BUTTON_GPIO = 16
+BUTTON_GPIO = 18
 USER_ID = 0
 url = "http://164.92.97.79/request_user_id/"
 
@@ -16,10 +16,10 @@ user_id_file_path = "user_id.txt"
 
 
 
-def request_user_id():
+def request_user_id(channel):
     try:
-        r = request.get(url, timeout=1) #wait for response for 1 second before timing out
-        r = response.json()
+        r = requests.get(url, timeout=1) #wait for response for 1 second before timing out
+        r = r.json()
         USER_ID = r["user_id"] #need to test if this works, or if I need matchdict or something
 
         #Save user_id for later use
@@ -30,22 +30,25 @@ def request_user_id():
                 pass
 
         f = open(user_id_file_path, "w")
-        f.write("USER_ID: " + USER_ID)
+        f.write("USER_ID: " + str(USER_ID))
         f.close()
         
 
     except requests.exceptions.Timeout:
-        print("request_user_id restful request timed out (No match ?)")
+        print("ERROR: request_user_id restful request timed out (No match ?)")
 
 
 if __name__ == "__main__":
-    try:
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)  #make it a pull up input
-                                                                    #this will be default 1, pulled down by button
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)  #make it a pull up input
+                                                                #this will be default 1, pulled down by button
 
-        GPIO.add_event_detect(BUTTON_GPIO, GPIO.FALLING, 
+    GPIO.add_event_detect(BUTTON_GPIO, GPIO.FALLING, 
                 callback=request_user_id, bouncetime=100)   #setup interrupt on pulldown to send request
-        r = request.get(url, timeout=1) #wait for response for 1 second before timing out
+
+    try:
+        while(True):
+            continue
+        #r = request.get(url, timeout=1) #wait for response for 1 second before timing out
     except KeyboardInterrupt:
         exit
